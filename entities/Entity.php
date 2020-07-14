fe4d713bd159698f7838b9bd2837e29a52678301
stasya0903 <stasya0903@mail.ru>
2020-07-14 10:48:08 -0500

    set up MVC-like framework for project

---
+++ entities/Entity.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\entities;
+
+
+/**
+ * Class Entity
+ * @package App\entities
+ *
+ * @property int $id
+ */
+
+abstract class Entity
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\exceptions\AddressNotFoundException;
+use App\main\AppCall;
+use App\main\App;
+use App\services\renders\TwigRender;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+$config = include dirname(__DIR__) . '/main/config.php';
+AppCall::call()->getConfig($config);
+App::call()->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
